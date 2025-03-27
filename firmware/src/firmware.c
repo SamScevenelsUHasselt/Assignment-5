57009177ecda5914727a2e7aef0c7c9a004f7764
VastePC <sam.scevenels@student.uhasselt.be>
2025-03-27 17:18:19 +0100

    push to roger

--- firmware/src/firmware.c
+++ firmware/src/firmware.c
@@ -113,20 +113,20 @@ int main(void) {
     }
 
     //Store header
-    store_byte(&current, header.magic[0], &image_chunk_index);
-    store_byte(&current, header.magic[1], &image_chunk_index);
-    store_byte(&current, header.magic[2], &image_chunk_index);
-    store_byte(&current, header.magic[3], &image_chunk_index);
-    store_byte(current,(header.width&0xFF000000)<<24,image_chunk_index);
-    store_byte(current,(header.width&0x00FF0000)<<16,image_chunk_index);
-    store_byte(current,(header.width&0x0000FF00)<<8,image_chunk_index);
-    store_byte(current,(header.width&0x000000FF),image_chunk_index);
-    store_byte(current,(header.height&0xFF000000)<<24,image_chunk_index);
-    store_byte(current,(header.height&0x00FF0000)<<16,image_chunk_index);
-    store_byte(current,(header.height&0x0000FF00)<<8,image_chunk_index);
-    store_byte(current,(header.height&0x000000FF),image_chunk_index);
-    store_byte(&current, header.channels, &image_chunk_index);
-    store_byte(&current, header.colorspace, &image_chunk_index);
+    store_byte(&current, header.magic[0],               &image_chunk_index);
+    store_byte(&current, header.magic[1],               &image_chunk_index);
+    store_byte(&current, header.magic[2],               &image_chunk_index);
+    store_byte(&current, header.magic[3],               &image_chunk_index);
+    store_byte(&current,(header.width&0xFF000000)<<24,  &image_chunk_index);
+    store_byte(&current,(header.width&0x00FF0000)<<16,  &image_chunk_index);
+    store_byte(&current,(header.width&0x0000FF00)<<8,   &image_chunk_index);
+    store_byte(&current,(header.width&0x000000FF),      &image_chunk_index);
+    store_byte(&current,(header.height&0xFF000000)<<24, &image_chunk_index);
+    store_byte(&current,(header.height&0x00FF0000)<<16, &image_chunk_index);
+    store_byte(&current,(header.height&0x0000FF00)<<8,  &image_chunk_index);
+    store_byte(&current,(header.height&0x000000FF),     &image_chunk_index);
+    store_byte(&current, header.channels,               &image_chunk_index);
+    store_byte(&current, header.colorspace,                 &image_chunk_index);
 
     /* Loop over pixels */
     for(uint8_t h=0;h<C_HEIGHT;h++) {
