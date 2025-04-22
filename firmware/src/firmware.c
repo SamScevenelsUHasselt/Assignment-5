453981ba594ca9f06971256afbe53fee1e5a027d
Sam <Sam.Scevenels@student.uhasselt.be>
2025-04-22 09:54:23 +0200

    push to roger

--- firmware/src/firmware.c
+++ firmware/src/firmware.c
@@ -56,10 +56,10 @@ void store_byte(struct qoi_image_chunk **current, const unsigned char to_store, 
 
 int main(void) {
 
-    print_str("Sensor Height: ");
-    print_dec(SENSOR_get_height());
-    print_str("Sensor Width: ");
-    print_dec(SENSOR_get_width());
+    //print_str("Sensor Height: ");
+    //print_dec(SENSOR_get_height());
+    //print_str("Sensor Width: ");
+    //print_dec(SENSOR_get_width());
 
     unsigned char r, r_prev = 0;
     unsigned char g, g_prev = 0;
