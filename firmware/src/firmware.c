e1dee97f13c52c4e34773f90ef7b68876a1ba7ae
Sam <Sam.Scevenels@student.uhasselt.be>
2025-04-22 11:25:06 +0200

    push to roger

--- firmware/src/firmware.c
+++ firmware/src/firmware.c
@@ -129,10 +129,10 @@ int main(void) {
             b = (unsigned char)(value >> 8);
             a = (unsigned char)(value);
 
-            //int pixel = (r << 24) | (g << 16) | (b << 8) | a;
-            //print_str("Pixel: X");
-            //print_hex(pixel,8);
-            //print_str("\n");
+            int pixel = (r << 24) | (g << 16) | (b << 8) | a;
+            print_str("Pixel: X");
+            print_hex(pixel,8);
+            print_str("\n");
 
             //STEP 1 ------ check if equal to previous pixel ---------------------------------------------------------------------------------------------------------------------
             if (r == r_prev && g == g_prev && b == b_prev && a == a_prev) {
